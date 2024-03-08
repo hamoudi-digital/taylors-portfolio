6f5c5f8f49704ac17c531e0abd6232b92384c434
huspain <hussainhamoudi@gmail.com>
2024-03-07 19:00:35 -0500

    state validation

--- src/pages/PortfolioSingle.js
+++ src/pages/PortfolioSingle.js
@@ -4,27 +4,19 @@ import React, { useEffect } from "react";
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
 import ContactCallout from "../components/ContactCallout";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import PageNotFound from "./PageNotFound";
 
 function PortfolioSingle() {
     const location = useLocation();
+    
     var state;
     if (location.state) {
         state = location.state;
     }
     else {
-        state = {
-            id: 0,
-            image: {
-                fullSrc: 'http://images.ctfassets.net/pehpejwgu7h3/58yyJfMTv9QfWV9U4lpBHX/97c6604e1abdd50edaad9759001e1dee/5D15BD16-E3FF-4807-8244-548063A73C68.png',
-            },
-            title: 'Introducing Taylor',
-            category: 'Character Design',
-            description: '',
-            externalLink: ''
-        }
+        state = false;
     }
 
     let renderOptions = {
@@ -35,45 +27,52 @@ function PortfolioSingle() {
     }
 
     useEffect (() => {
-        window.scrollTo({top: 0, left: 0});
-        let descriptionHtml = documentToHtmlString(state.description, renderOptions);
-        document.querySelector('.portfolio-single .description').innerHTML = descriptionHtml;
+        if (state) {
+            window.scrollTo({top: 0, left: 0});
+            let descriptionHtml = documentToHtmlString(state.description, renderOptions);
+            document.querySelector('.portfolio-single .description').innerHTML = descriptionHtml;
+        }
     });
 
-    return (
-        <main>
-            <Navigation />
-            <div className="portfolio-single section">
-                <div className="container">
-                     <div className="row">
-                        <div className="col-12">
-                            <div className="breadcrumbs">
-                                <em><Link to="/">Home</Link> {'>'} {state.title}
-                                </em>
+    if (state) {
+        return (
+            <main>
+                <Navigation />
+                <div className="portfolio-single section">
+                    <div className="container">
+                         <div className="row">
+                            <div className="col-12">
+                                <div className="breadcrumbs">
+                                    <em><Link to="/">Home</Link> {'>'} {state.title}
+                                    </em>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6 text-center featured-image" id="featured-image">
+                                {state.externalLink ? 
+                                    <a href={state.externalLink} target="_blank"> 
+                                        <img src={state.image.fullSrc} />
+                                    </a>
+                                    : 
+                                    <img src={state.image.fullSrc} />}
+                            </div>
+                            <div className="col-md-6 text-left">
+                                <h1>{state.title}</h1>
+                                <div className="description"></div>
+                                {state.externalLink ? <a href={state.externalLink} className="button" target="_blank">Learn More</a> : ''}
                             </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-6 text-center featured-image" id="featured-image">
-                            {state.externalLink ? 
-                                <a href={state.externalLink} target="_blank"> 
-                                    <img src={state.image.fullSrc} />
-                                </a>
-                                : 
-                                <img src={state.image.fullSrc} />}
-                        </div>
-                        <div className="col-md-6 text-left">
-                            <h1>{state.title}</h1>
-                            <div className="description"></div>
-                            {state.externalLink ? <a href={state.externalLink} className="button" target="_blank">Learn More</a> : ''}
-                        </div>
-                    </div>
                 </div>
-            </div>
-            <ContactCallout />
-            <Footer />
-        </main>
-    );
+                <ContactCallout />
+                <Footer />
+            </main>
+        );
+    }
+    else {
+        return(<PageNotFound />);
+    }
 }
 
 export default PortfolioSingle;
